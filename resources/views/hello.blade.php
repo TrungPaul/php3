2c812878c1b46a6fb5a6c20e2798379b3b88f458
Paul trung <trungndph06499@fpt.edu.vn>
2019-07-13 20:16:53 +0700

    Lab2

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<table class="table">
+  <thead class="thead-dark">
+  	
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">age</th>
+      <th scope="col">add</th>
+  
+    </tr>
+  </thead>
+  <tbody>
+  	
+    <tr>
+      			<td>{{$ten}}</td>
+				<td>{{$add}}</td>
+				<td>{{$age}}</td>
+    </tr>
+  
+  </tbody>
+</table>
+</body>
+</html>
